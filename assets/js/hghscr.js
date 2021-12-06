428de26c06bdbff6d828fb1c00b8c413ecfb1f7d
David Manriquez <dmanriqu@my.chemeketa.edu>
2021-12-05 23:27:29 -0800

    finishing touches

--- assets/js/hghscr.js
+++ assets/js/hghscr.js
@@ -25,9 +25,8 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign("./Highscores.html");
+    window.location.assign("/");
 
-    console.log(highScores);
 
 };
 
