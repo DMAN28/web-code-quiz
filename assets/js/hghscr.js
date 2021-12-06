94799c011feab8770cb00bab7489864dba2c77c5
David Manriquez <dmanriqu@my.chemeketa.edu>
2021-12-05 21:01:56 -0800

    High score

--- assets/js/hghscr.js
+++ assets/js/hghscr.js
@@ -1,10 +1,9 @@
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
-const mostRecentScore = document.getElementById("mostRecentScore");
-finalScore.innerText = mostRecentScore;
-
 const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
@@ -14,4 +13,21 @@ username.addEventListener("keyup", () => {
 saveHighScore = e =>{
     console.log("clicked the save button");
     e.preventDefault();
-}
+
+    const score= {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    }
+    highScores.push(score);
+    highScores.sort((a,b) => { b.score - a.score; });
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+
+    console.log(highScores);
+
+};
+
+
+
+
